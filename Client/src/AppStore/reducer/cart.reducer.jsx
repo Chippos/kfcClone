6acc1219660960c30ef10fae92532ed1ba2fbb4f
AliXhan-Pixels <66645628+Alixhan-pixel@users.noreply.github.com>
2023-05-30 18:25:50 +0500

    enchanced Code & remove extra line

--- Client/src/AppStore/reducer/cart.reducer.jsx
+++ Client/src/AppStore/reducer/cart.reducer.jsx
@@ -10,29 +10,8 @@ export default function (state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
     case "ADD_TO_CART":
-     {
-      if (state.addedItems.length === 0) {
-        return {
-          ...state,
-          data: payload,
-          addedItems: [...state.addedItems, { ...payload, quantity: 1 }],
-          isLoading: false,
-          quantity: (state.quantity += 1),
-        };
-      } else if (state.addedItems.length > 0) {
-        //check if the action id exists in the addedItems
-        let existed_item = state.addedItems.find(
-          (item) => payload._id === item._id
-        );
-        if (existed_item) {
-          existed_item.quantity += 1;
-          return {
-            ...state,
-            data: payload,
-            addedItems: [...state.addedItems],
-            isLoading: false,
-          };
-        } else {
+      {
+        if (state.addedItems.length === 0) {
           return {
             ...state,
             data: payload,
@@ -40,10 +19,31 @@ export default function (state = initialState, action) {
             isLoading: false,
             quantity: (state.quantity += 1),
           };
+        } else if (state.addedItems.length > 0) {
+          //check if the action id exists in the addedItems
+          let existed_item = state.addedItems.find(
+            (item) => payload._id === item._id
+          );
+          if (existed_item) {
+            existed_item.quantity += 1;
+            return {
+              ...state,
+              data: payload,
+              addedItems: [...state.addedItems],
+              isLoading: false,
+            };
+          } else {
+            return {
+              ...state,
+              data: payload,
+              addedItems: [...state.addedItems, { ...payload, quantity: 1 }],
+              isLoading: false,
+              quantity: (state.quantity += 1),
+            };
+          }
         }
       }
-     }
-     break;
+      break;
     case "ADD_CART_INC":
       {
         let existed_item = state.addedItems.find(
@@ -51,13 +51,6 @@ export default function (state = initialState, action) {
         );
         if (existed_item) {
           existed_item.quantity += 1;
-          return {
-            ...state,
-            data: payload,
-            addedItems: [...state.addedItems],
-            isLoading: false,
-          };
-        } else {
           return {
             ...state,
           };
@@ -71,33 +64,28 @@ export default function (state = initialState, action) {
         );
         if (existed_item) {
           existed_item.quantity -= 1;
-          return {
-            ...state,
-            data: payload,
-            addedItems: [...state.addedItems],
-            isLoading: false,
-          };
-        } else {
+          console.log({ dec: existed_item });
           return {
             ...state,
           };
         }
       }
       break;
-      case "ADD_CART_DEL":
+    case "ADD_CART_DEL":
       {
         let existed_item = state.addedItems.filter(
           (item) => payload._id !== item._id
         );
-        console.log(existed_item)
+        console.log(existed_item);
         return {
           ...state,
           data: payload,
           addedItems: [...existed_item],
           isLoading: false,
-          quantity: (state.quantity-=1),
+          quantity: (state.quantity -= 1),
         };
       }
+      break;
     default:
       return state;
   }
